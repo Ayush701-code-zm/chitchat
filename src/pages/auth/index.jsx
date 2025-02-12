88b23ddb2b48344a3aa243203fbcd898e78656b5
Ayush701-code-zm <h3yayush@gmail.com>
2025-02-12 20:52:12 +0530

    ok

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const index = () => {
-  return <div></div>;
+  return (
+    <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+      <div className=" h-[80vh] bg-white border-2"></div>
+      Au
+    </div>
+  );
 };
 
 export default index;
