8cf77d82b4e83bae069f962180ca59b19761eaaa
Ayush701-code-zm <h3yayush@gmail.com>
2025-03-04 04:23:17 +0530

    ok

--- src/components/common/contact-list/index.jsx
+++ src/components/common/contact-list/index.jsx
@@ -22,53 +22,60 @@ const ContactList = ({ contacts, isChannel = false }) => {
 
   return (
     <div className="mt-5">
-      {contacts.map((contact) => (
-        <div
-          key={contact._id}
-          className={`pl-10 py-2  transition-all duration-300 cursor-pointer ${
-            selectedChatData && selectedChatData._id === contact._id
-              ? "bg-[#8417ff] hover:bg-[#8417ff]"
-              : "hover:bg-[#f1f1f111] "
-          }`}
-          onClick={() => handleClick(contact)}
-        >
-          <div className="flex gap-5 items-center justify-start text-neutral-300">
-            {!isChannel && (
-              <Avatar className="h-10 w-10 ">
-                {contact.image && (
-                  <AvatarImage
-                    src={`${HOST}/${contact.image}`}
-                    alt="profile"
-                    className="rounded-full bg-cover h-full w-full"
-                  />
-                )}
+      {contacts.map((contact) => {
+        const isSelected = selectedChatData?._id === contact._id;
 
-                <AvatarFallback
-                  className={`uppercase ${
-                    selectedChatData && selectedChatData._id === contact._id
-                      ? "bg-[#ffffff22] border border-white/50"
-                      : getColor(contact.color)
-                  } h-10 w-10 flex items-center justify-center rounded-full`}
+        return (
+          <div
+            key={contact._id}
+            className={`pl-10 py-2 transition-all duration-300 cursor-pointer ${
+              isSelected
+                ? "bg-[#8417ff] hover:bg-[#8417ff]"
+                : "hover:bg-[#f1f1f111]"
+            }`}
+            onClick={() => handleClick(contact)}
+          >
+            <div className="flex gap-5 items-center justify-start text-neutral-300">
+              {!isChannel && (
+                <Avatar className="h-10 w-10">
+                  {contact.image ? (
+                    <AvatarImage
+                      src={`${HOST}/${contact.image}`}
+                      alt="profile"
+                      className="rounded-full bg-cover h-full w-full"
+                    />
+                  ) : (
+                    <AvatarFallback
+                      className={`uppercase ${
+                        isSelected
+                          ? "bg-[#ffffff22] border border-white/50"
+                          : getColor(contact.color) || "bg-gray-300"
+                      } h-10 w-10 flex items-center justify-center rounded-full`}
+                    >
+                      {contact.firstName
+                        ? contact.firstName[0].toUpperCase()
+                        : "N/A"}
+                    </AvatarFallback>
+                  )}
+                </Avatar>
+              )}
+              {isChannel && (
+                <div
+                  className="bg-[#ffffff22] h-10 w-10 flex items-center justify-center rounded-full"
+                  title="Channel"
                 >
-                  {contact.firstName.split("").shift()}
-                </AvatarFallback>
-              </Avatar>
-            )}
-            {isChannel && (
-              <div
-                className={` bg-[#ffffff22] h-10 w-10 flex items-center justify-center rounded-full`}
-              >
-                #
-              </div>
-            )}
-            {isChannel ? (
-              <span>{contact.name}</span>
-            ) : (
-              <span>{`${contact.firstName} ${contact.lastName}`}</span>
-            )}
+                  #
+                </div>
+              )}
+              {isChannel ? (
+                <span>{contact.name}</span>
+              ) : (
+                <span>{`${contact.firstName} ${contact.lastName}`}</span>
+              )}
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
